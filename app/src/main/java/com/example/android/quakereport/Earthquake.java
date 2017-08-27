56f031ab68de773ffc3bd6252aa94b66c175298b
ridhwandaud <m.ridhwan.daud@gmail.com>
2017-08-27 19:52:22 +0800

    add click listener to open website

--- app/src/main/java/com/example/android/quakereport/Earthquake.java
+++ app/src/main/java/com/example/android/quakereport/Earthquake.java
@@ -12,11 +12,14 @@ public class Earthquake {
 
     private long mTimeInMiliseconds;
 
+    private  String mUrl;
 
-    public Earthquake(double magnitude, String location, long timeInMiliseconds){
+
+    public Earthquake(double magnitude, String location, long timeInMiliseconds, String url){
         mMagnitude = magnitude;
         mLocation = location;
         mTimeInMiliseconds = timeInMiliseconds;
+        mUrl = url;
     }
 
     public double getMagnitude(){
@@ -30,4 +33,6 @@ public class Earthquake {
     public long getTimeInMiliseconds(){
         return mTimeInMiliseconds;
     }
+
+    public  String getUrl(){ return  mUrl; }
 }
